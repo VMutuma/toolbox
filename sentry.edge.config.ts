f70a780bd092d8d05f1a8efe4e4b9d8518f41566
Vmutuma <victormwenda39@gmail.com>
2026-06-22 15:37:05 +0300

    Squashed 'Portfolio-Website/' content from commit 98ffacd
    git-subtree-dir: Portfolio-Website
    git-subtree-split: 98ffacdb1c98360763f2fdc1072938f0e131ff22

---
+++ sentry.edge.config.ts
@@ -0,0 +1,16 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://188dcc6df396ca36e1d13ce73d1b96f1@o4507535381692416.ingest.de.sentry.io/4507535394013264",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
